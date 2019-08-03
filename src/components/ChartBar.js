3826a29df12913f832f2a0c3139da2c0c846e95b
Vladislav <zippo585@gmail.com>
2019-08-03 16:30:00 +0500

    few fixes

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import Chart from "chart.js";
 
 class ChartBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chartData: {}
-    };
-  }
+
 
 
    createChart(self){
     const myChartRef = self.chartRef.current.getContext("2d");
   
-    console.log(self.state.chartData.length, "check");
     new Chart(myChartRef, {
       type: "bar",
       data: self.props.chartData,
@@ -57,7 +51,6 @@ class ChartBar extends Component {
   }
 
   componentDidUpdate(prevProps){
-    console.log(prevProps, "prevProps");
 
     if(prevProps.chartData !== this.props.chartData){
      var self = this;
